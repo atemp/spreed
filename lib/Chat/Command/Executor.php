7b1b9b1ec66b71991f653197f2d1feab7162df1d
Daniel Calviño Sánchez <danxuliu@gmail.com>
2020-10-03 22:00:11 +0200

    Do not list unavailable commands in "/help"
    If a user is not going to be able to use a command it should not be
    listed when calling "/help".
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- lib/Chat/Command/Executor.php
+++ lib/Chat/Command/Executor.php
@@ -87,7 +87,7 @@ class Executor {
 		return true;
 	}
 
-	public function exec(Room $room, IComment $message, Command $command, string $arguments): void {
+	public function exec(Room $room, IComment $message, Command $command, string $arguments, Participant $participant): void {
 		try {
 			$command = $this->commandService->resolveAlias($command);
 		} catch (DoesNotExistException $e) {
@@ -120,7 +120,7 @@ class Executor {
 		$message->setVerb('command');
 	}
 
-	protected function execHelp(Room $room, IComment $message, string $arguments): string {
+	protected function execHelp(Room $room, IComment $message, string $arguments, Participant $participant): string {
 		if ($arguments !== '' && $arguments !== 'help') {
 			return $this->execHelpSingleCommand($room, $message, $arguments);
 		}
@@ -132,7 +132,8 @@ class Executor {
 			if ($command->getApp() !== '') {
 				$response = $this->execHelpSingleCommand($room, $message, $command->getApp() . ' ' . $command->getCommand());
 			} else {
-				if ($command->getCommand() === 'help' || strpos($command->getScript(),'alias:') !== false) {
+				if ($command->getCommand() === 'help' || strpos($command->getScript(),'alias:') !== false ||
+						!$this->isCommandAvailableForParticipant($command, $participant)) {
 					continue;
 				}
 				$response = $this->execHelpSingleCommand($room, $message, $command->getCommand());
